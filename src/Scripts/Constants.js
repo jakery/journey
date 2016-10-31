83949fce03bf42e41a8b904bc0cf731f68de1c96
Jacob King <jacobking2@gmail.com>
2016-10-31 10:41:12 -0400

    Added some constants.

--- src/Scripts/Constants.js
+++ src/Scripts/Constants.js
@@ -1,5 +1,15 @@
-define('Constants', [], () => ({
+define('Constants', ['./TileCodes'], TileCodes => ({
+  tileCodes: TileCodes,
   gameModes: {
     title: 0,
+    password: 1,
+    normal: 2,
+    credits: 3,
   },
+  directions: {
+    up: 0,
+    right: 1,
+    down: 2,
+    left: 3
+  }
 }));
