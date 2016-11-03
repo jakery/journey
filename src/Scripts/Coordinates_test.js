4bbddb5e2b3749c9c4964e24d19f69cdcfe275b4
Jacob King <jacobking2@gmail.com>
2016-11-03 15:58:20 -0400

    Added preliminary tests for Coordinates and Utility.

---
+++ src/Scripts/Coordinates_test.js
@@ -0,0 +1,16 @@
+let Coordinates = require('./Coordinates');
+describe('CoordinatesTest', () => {
+  describe('Coordinates', function () {
+    it('should create coordinates.', function () {
+      let c = new Coordinates(0, 0);
+      assert.equal(0, c.x);
+      assert.equal(0, c.y);
+    });
+
+    it('should have null X and Y properties.', function () {
+      let c = new Coordinates(Infinity, NaN);
+      assert.equal(null, c.x);
+      assert.equal(null, c.y);
+    });
+  });
+});
