0e3c5e7240647081e313c18a7b1736d25a0c76f4
Jacob King <jacobking2@gmail.com>
2016-11-04 11:58:22 -0400

    More unit tests for the Dom helper.

--- src/Tests/Utility_test.js
+++ src/Tests/Utility_test.js
@@ -6,12 +6,12 @@ describe('Utility', () => {
     it('should return 1', function () {
       const sprite1 = new Coordinates(-1, -1);
       const sprite2 = new Coordinates(-2, -1);
-      assert.equal(1, Utility.tileDistanceBetween(sprite1, sprite2));
+      assert.equal(Utility.tileDistanceBetween(sprite1, sprite2), 1);
     });
     it('should return 5', function () {
       const sprite1 = new Coordinates(10, 50);
       const sprite2 = new Coordinates(8, 47);
-      assert.equal(5, Utility.tileDistanceBetween(sprite1, sprite2));
+      assert.equal(Utility.tileDistanceBetween(sprite1, sprite2), 5);
     });
   });
   describe('areColliding', function () {
@@ -29,7 +29,7 @@ describe('Utility', () => {
 
     describe('new', function () {
       it('should be a new Dom object.', function () {
-        assert.equal('test div', myDom.element.innerHTML);
+        assert.equal(myDom.element.innerHTML, 'test div');
         assert.isTrue(typeof myDom.style === 'function');
       });
     });
@@ -37,26 +37,26 @@ describe('Utility', () => {
     describe('.style()', function () {
       it('should set the color style to red.', function () {
         myDom.style('color', 'red');
-        assert.equal('red', myDom.element.style.color);
+        assert.equal(myDom.element.style.color, 'red');
       });
       it('should output the element\'s CSSStyleDeclaration', function () {
         var theStyle = myDom.style();
-        assert.equal('CSSStyleDeclaration', theStyle.constructor.name);
+        assert.equal(theStyle.constructor.name, 'CSSStyleDeclaration');
       });
       it('should output the element\'s single style value.', function () {
         myDom.style('color', 'green');
         const theStyle = myDom.style('color');
-        assert.equal('green', theStyle);
+        assert.equal(theStyle, 'green');
       });
     });
 
     describe('.hide()', function () {
       it('should hide the element.', function () {
         myDom.hide();
-        assert.equal('hidden', myDom.element.style.visibility);
+        assert.equal(myDom.element.style.visibility, 'hidden');
       });
       it('should return the dom object.', function () {
-        assert.deepEqual(myDom, myDom.hide());
+        assert.deepEqual(myDom.hide(), myDom);
       });
     });
 
@@ -70,11 +70,26 @@ describe('Utility', () => {
       });
     });
 
-
     describe('.ready()', function () {
       it('should execute a callback when the document is loaded.', function () {
-        assert.isTrue(false);
+        let myNumber = 1;
+        let myDocument = new Utility.Dom(document);
+        let callbackExecuted = false;
+
+        let myCallback = function myCallback() {
+          callbackExecuted = true;
+        };
+        myDocument.ready(myCallback);
+
+        // Manually trigger DOMContentLoaded.
+        var DOMContentLoaded_event = document.createEvent("Event")
+        DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true)
+        window.document.dispatchEvent(DOMContentLoaded_event);
+
+        assert.isTrue(callbackExecuted);
+
       });
+
       it('should return the dom object.', function () {
         assert.deepEqual(myDom, myDom.ready());
       });
