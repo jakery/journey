98f4c6a8e18532d59aa65dd36d3f76f4ad4f4719
Jacob King <jacobking2@gmail.com>
2016-11-07 10:42:31 -0500

    Modularized AwesomeError.

---
+++ src/Scripts/AwesomeError.js
@@ -0,0 +1,12 @@
+define('AwesomeError', ['./Utility'], Utility => function AwesomeError(data) {
+  this.data = data;
+  this.message = `YOU WIN!\n\nActually, you didn't win. You've encountered a bug that's broken the game. I was trying to make you feel better about it.\n\n` +
+    `Contact me and tell me, or else I'll never find out and this will never get fixed.\n\n` +
+    `Also, "It's broken" with no further information is worse than saying nothing at all. That's why you get crappy tech support at your job.\n\n` +
+    `Say what the specific problem is, and also say this stuff too:\n` +
+    `Attempted function : ${this.data.attemptedFunction} \n` +
+    `Level : ${typeof (this.data.level) !== 'undefined' ? this.data.level : 0}\n` +
+    `Player Coords : ${this.data.position.x},${this.data.position.y}\n` +
+    `Error Code : ${typeof (this.data.errorCode) !== 'undefined' ? this.data.errorCode : 'none'}\n\n`;
+  this.go = Utility.alert(this.message);
+});
---
+++ src/Tests/AwesomeError_test.js
@@ -0,0 +1,15 @@
+let AwesomeError = require('../Scripts/AwesomeError');
+describe('AwesomeError', function () {
+  describe('create', function () {
+    it('should create the awesome error message.', function () {
+      let awesomeError = new AwesomeError({
+        attemptedFunction: 'unitTest',
+        errorCode: '404',
+        position: {x: 0, y: 5},
+        level: 1,
+      });
+      assert.include(awesomeError.message, "Attempted function : unitTest");
+            assert.include(awesomeError.message, "unitTest");
+    });
+  });
+});
