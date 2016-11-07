75fec1d3c880e718e840be44b3ae9444c710656f
Jacob King <jacobking2@gmail.com>
2016-11-07 10:10:44 -0500

    Added simple tests to Stage object.

--- src/Tests/Stage_test.js
+++ src/Tests/Stage_test.js
@@ -1,11 +1,22 @@
 let Stage = require('../Scripts/Stage');
 describe('Stage', function () {
+  let gameCanvas = document.createElement('canvas');
+  gameCanvas.id = 'gameCanvas';
+  document.body.appendChild(gameCanvas);
+  let myStage = new Stage();
+  myStage.init();
   beforeEach(function () {
   });
-  describe('unit tests', function () {
-    it('should actually exist.', function () {
-      throw new Error('they don\'t.');
+  describe('.init()', function () {
+    it('should have a width and height.', function () {
+      assert.isNumber(myStage.width);
+      assert.isNumber(myStage.height);
     });
+    it('should have a playbox width and playbox height.', function () {
+      assert.isNumber(myStage.playboxWidth);
+      assert.isNumber(myStage.playboxHeight);
+    });
+
   });
 });
 
