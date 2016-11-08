acc40883aabad9812f3675a9605c7b09d4bf51ce
Jacob King <jacobking2@gmail.com>
2016-11-08 16:00:46 -0500

    Unit test game var.

--- src/Tests/Game_test.js
+++ src/Tests/Game_test.js
@@ -1,6 +1,7 @@
 let Game = require('../Scripts/Game');
 describe('Game', function () {
   describe('setLevelClock()', function () { });
+  let game = new Game();
   describe('returnToTitle()', function () { });
   describe('setDeadMessage()', function () { });
 });
