a017c5fd2681b4b239e24b2b24cc1dd889807016
Jacob King <jacobking2@gmail.com>
2016-11-08 14:57:14 -0500

    Fleshing out the Map object; added Object.assign() for importing the JSON.

--- src/Scripts/Map.js
+++ src/Scripts/Map.js
@@ -1,6 +1,9 @@
-define('Map', ['./Constants/Constants', './Coordinates'], (Constants, Coordinates) => function Map(masterGame) {
-  this.width = masterGame.map.width;
-  this.height = masterGame.map.height;
+define('Map', ['./Constants/Constants', './Coordinates'], (Constants, Coordinates) => function Map(map) {
+
+
+  this.width = null;
+  this.height = null;
+  this.tilesets = null;
 
   this.getTileIndexByCoords = function getTileIndexByCoords(x, y) {
     return (y * this.width) + x;
@@ -14,44 +17,39 @@ define('Map', ['./Constants/Constants', './Coordinates'], (Constants, Coordinate
       && coords.y < this.height;
   };
 
-  // Temporary function to lessen pain during the refactor.
-  this.wireUp = function wireUp(g) {
-    const game = g;
-    game.map.tileProperties = game.map.tilesets[0].tileproperties;
-
-    game.map.getTileIndexByCoords = (x, y) => this.getTileIndexByCoords(x, y);
-
-    game.map.getTileTypeByCoords = function getTileTypeByCoords(x, y) {
-      const arrayIndex = this.getTileIndexByCoords(x, y);
-      return this.layers[0].data[arrayIndex];
-    };
-
-    game.map.getCoordsByTileIndex = function getCoordsByTileIndex(i) {
-      return new Coordinates(
-        i % game.map.width,
-        Math.floor(i / game.map.width)
-      );
-    };
-
-    game.map.changeTileType = function changeTileType(x, y, type) {
-      const arrayIndex = this.getTileIndexByCoords(x, y);
-      game.map.layers[0].data[arrayIndex] = type;
-    };
-
-    game.map.isInBounds = coords => this.isInBounds(coords);
-
-    // TODO: Make this a function.
-    for (let i = 0; i < game.map.tilesets.length; i += 1) {
-      game.map.tilesets[i].indexWidth =
-        game.map.tilesets[0].imagewidth / game.map.tilesets[0].tilewidth;
-      game.map.tilesets[i].indexHeight =
-        game.map.tilesets[0].imageheight / game.map.tilesets[0].tileheight;
-    }
-
-    // TODO: Refactor these.
-    game.map.mdMap = this.getMultiDimensionalMap(game.map.layers[0].data, game.map.width);
-
-    game.map.pixelWidth = game.map.width * Constants.baseUnit;
-    game.map.pixelHeight = game.map.height * Constants.baseUnit;
+  this.getCoordsByTileIndex = function getCoordsByTileIndex(i) {
+    return new Coordinates(
+      i % this.width,
+      Math.floor(i / this.width)
+    );
   };
+
+  this.getTileTypeByCoords = function getTileTypeByCoords(x, y) {
+    const arrayIndex = this.getTileIndexByCoords(x, y);
+    return this.layers[0].data[arrayIndex];
+  };
+
+  this.changeTileType = function changeTileType(x, y, type) {
+    const arrayIndex = this.getTileIndexByCoords(x, y);
+    this.layers[0].data[arrayIndex] = type;
+  };
+
+  Object.assign(this, map);
+
+
+  // TODO: Refactor these.
+  this.mdMap = this.getMultiDimensionalMap(this.layers[0].data, this.width);
+
+  this.drawWidth = this.pixelWidth = this.width * Constants.baseUnit;
+  this.drawHeight = this.pixelHeight = this.height * Constants.baseUnit;
+
+
+  // TODO: Make this a function.
+  for (let i = 0; i < this.tilesets.length; i += 1) {
+    this.tilesets[i].indexWidth =
+      this.tilesets[0].imagewidth / this.tilesets[0].tilewidth;
+    this.tilesets[i].indexHeight =
+      this.tilesets[0].imageheight / this.tilesets[0].tileheight;
+  }
+
 });
