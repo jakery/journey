df29f0d6f634e96f17589a05c8e7d3045156d687
Jacob King <jacobking2@gmail.com>
2016-11-11 12:32:59 -0500

    Added todos.

--- src/Scripts/Utility/Utility.js
+++ src/Scripts/Utility/Utility.js
@@ -1,5 +1,13 @@
 define('Utility', ['../Constants/ErrorMessages', './String', './Array'], (ErrorMessages, UtilityString, UtilityArray) => {
   const Utility = function Utility() {
+
+    // Math
+    // TODO: Move to Math.js
+    this.math = {
+      toRadians: degrees => (degrees * Math.PI) / 180,
+      toDegrees: radians => (radians * 180) / Math.PI,
+    };
+
     this.tileDistanceBetween = function tileDistanceBetween(coords1, coords2) {
       const xDist = Math.abs(coords1.x - coords2.x);
       const yDist = Math.abs(coords1.y - coords2.y);
@@ -13,6 +21,10 @@ define('Utility', ['../Constants/ErrorMessages', './String', './Array'], (ErrorM
     this.areSpritesColliding = function areSpritesColliding(s1, s2) {
       return this.areColliding(s1.position, s2.position);
     };
+
+
+    // Polyfills
+    // TODO: Move to Polyfills.js
 
     // Shim for missing console object.
     this.console = window.console || {
