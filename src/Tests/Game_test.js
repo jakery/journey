e2ef66495965eeb24a567b023b554b15dd88bb29
Jacob King <jacobking2@gmail.com>
2016-11-11 13:09:34 -0500

    What

--- src/Tests/Game_test.js
+++
@@ -1,9 +0,0 @@
-let Game = require('../Scripts/Game');
-describe('Game', function () {
-  let game = new Game();
-  describe('resetLevelVariables()', function () { });
-  describe('returnToTitle()', function () { });
-  describe('setDeadMessage()', function () { });
-  describe('setProperties()', function () { });
-  describe('nextLevel()', function () { });
-});
