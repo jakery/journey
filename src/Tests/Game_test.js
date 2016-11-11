a90375764a2f1fd2ac503603f8c726e2ca61974e
Jacob King <jacobking2@gmail.com>
2016-11-11 12:44:54 -0500

    Test for level data in game object.

--- src/Tests/Game_test.js
+++ src/Tests/Game_test.js
@@ -1,6 +1,13 @@
 let Game = require('../Scripts/Game');
 describe('Game', function () {
   let game = new Game();
+  describe('properties', function () {
+    it('should have all maps in this.levels', function () {
+      assert.isNotNull(game.levels);
+      assert.isDefined(game.levels['0']);
+      assert.equal(game.levels['0'].width, 15);
+    });
+  });
   describe('resetLevelVariables()', function () { });
   describe('returnToTitle()', function () { });
   describe('setDeadMessage()', function () { });
