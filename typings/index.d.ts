0749da23abdf5e4aeaefb052ee37dc86a9e93267
Jacob King <jacobking2@gmail.com>
2016-11-14 15:25:05 -0500

    A billion little typings.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="globals/karma/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/require/index.d.ts" />
+/// <reference path="modules/chai/index.d.ts" />
 /// <reference path="modules/webpack/index.d.ts" />
