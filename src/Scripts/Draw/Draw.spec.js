10461cb6258526b698f1549645cadf8c252238b9
Jacob King <jacobking2@gmail.com>
2016-11-14 14:52:44 -0500

    Linted Draw tests.

--- src/Scripts/Draw/Draw.spec.js
+++ src/Scripts/Draw/Draw.spec.js
@@ -1,25 +1,28 @@
-let Coordinates = require('../Coordinates');
-let Draw = require('./Draw');
-let testMap = require('json!../Map/Mocks/testMap.json');
-describe('Draw', function () {
-  describe('tileIsInDrawBounds', function () {
-    var canvas = document.createElement('canvas');
-    let game = {
+/* eslint-disable prefer-arrow-callback */
+const Coordinates = require('../Coordinates');
+const Draw = require('./Draw');
+// eslint-disable-next-line
+const testMap = require('json!../Map/Mocks/testMap.json');
+
+describe('Draw', function DrawTests() {
+  describe('tileIsInDrawBounds', function tileIsInDrawBounds() {
+    const canvas = document.createElement('canvas');
+    const game = {
       map: testMap,
     };
-    let stage = {
+    const stage = {
       playboxWidth: 15 * 32,
       playboxHeight: 12 * 32,
       gameCanvas: canvas,
     };
-    let player = {};
+    const player = {};
     const draw = new Draw(game, stage, player);
-    it('should return true', function () {
-      let coords = new Coordinates(64, 64);
+    it('should return true', function test() {
+      const coords = new Coordinates(64, 64);
       assert.isTrue(draw.tileIsInDrawBounds(coords));
     });
-    it('should return false', function () {
-      let coords = new Coordinates(-32, 10000);
+    it('should return false', function test() {
+      const coords = new Coordinates(-32, 10000);
       assert.isFalse(draw.tileIsInDrawBounds(coords));
     });
   });
