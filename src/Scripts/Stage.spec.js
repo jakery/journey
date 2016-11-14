ce5c14338a342933c015e115af710f73928d6399
Jacob King <jacobking2@gmail.com>
2016-11-14 15:06:32 -0500

    Linted Stage tests.

--- src/Scripts/Stage.spec.js
+++ src/Scripts/Stage.spec.js
@@ -1,22 +1,23 @@
-let Stage = require('../Scripts/Stage');
-describe('Stage', function () {
-  let gameCanvas = document.createElement('canvas');
+/* eslint-disable prefer-arrow-callback */
+const Stage = require('../Scripts/Stage');
+
+describe('Stage', function StageTests() {
+  const gameCanvas = document.createElement('canvas');
   gameCanvas.id = 'gameCanvas';
   document.body.appendChild(gameCanvas);
-  let myStage = new Stage();
+  const myStage = new Stage();
   myStage.init();
-  beforeEach(function () {
+  beforeEach(function beforeEach() {
   });
-  describe('.init()', function () {
-    it('should have a width and height.', function () {
+  describe('.init()', function init() {
+    it('should have a width and height.', function test() {
       assert.isNumber(myStage.width);
       assert.isNumber(myStage.height);
     });
-    it('should have a playbox width and playbox height.', function () {
+    it('should have a playbox width and playbox height.', function test() {
       assert.isNumber(myStage.playboxWidth);
       assert.isNumber(myStage.playboxHeight);
     });
-
   });
 });
 
