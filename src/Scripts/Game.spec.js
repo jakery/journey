7cff5d23f47ab201d899ee02e70c8002cd4ca16b
Jacob King <jacobking2@gmail.com>
2016-11-14 15:34:09 -0500

    Linted Game tests.

--- src/Scripts/Game.spec.js
+++ src/Scripts/Game.spec.js
@@ -1,16 +1,24 @@
-let Game = require('../Scripts/Game');
-describe('Game', function () {
-  let game = new Game();
-  describe('properties', function () {
-    it('should have all maps in this.levels', function () {
+/* eslint-disable prefer-arrow-callback */
+const Game = require('../Scripts/Game');
+
+describe('Game', function GameTests() {
+  const game = new Game();
+  describe('properties', function properties() {
+    it('should have all maps in this.levels', function test() {
       assert.isNotNull(game.levels);
       assert.isDefined(game.levels['0']);
       assert.equal(game.levels['0'].width, 15);
     });
   });
-  describe('resetLevelVariables()', function () { });
-  describe('returnToTitle()', function () { });
-  describe('setDeadMessage()', function () { });
-  describe('setProperties()', function () { });
-  describe('nextLevel()', function () { });
+  describe('resetLevelVariables()', function resetLevelVariables() { });
+  describe('returnToTitle()', function returnToTitle() { });
+  describe('setDeadMessage()', function setDeadMessage() { });
+  describe('setProperties()', function setProperties() {
+    game.setProperties({ foo: 'bar' });
+    assert.equal(game.foo, 'bar');
+  });
+  describe('restartLevel()', function restartLevel() {
+    // game.restartLevel();
+    // TODO: Add spy for game.loadMap(level)
+  });
 });
