873385cd08c996cde7ba9384f5b19ad68015ac20
Jacob King <jacobking2@gmail.com>
2016-11-14 16:08:45 -0500

    Linted AwesomeError tests.

--- src/Scripts/AwesomeError.spec.js
+++ src/Scripts/AwesomeError.spec.js
@@ -1,21 +1,23 @@
-let AwesomeError = require('../Scripts/AwesomeError');
-describe('AwesomeError', function () {
-  describe('create', function () {
-    it('should create the awesome error message.', function () {
-      let awesomeError = new AwesomeError({
+/* eslint-disable prefer-arrow-callback */
+const AwesomeError = require('../Scripts/AwesomeError');
+
+describe('AwesomeError', function AwesomeErrorTests() {
+  describe('create', function create() {
+    it('should create the awesome error message.', function test() {
+      const awesomeError = new AwesomeError({
         attemptedFunction: 'unitTest',
         errorCode: '404',
-        position: {x: 0, y: 5},
+        position: { x: 0, y: 5 },
         level: 1,
       });
-      assert.include(awesomeError.message, "Attempted function : unitTest");
-      assert.include(awesomeError.message, "unitTest");
+      assert.include(awesomeError.message, 'Attempted function : unitTest');
+      assert.include(awesomeError.message, 'unitTest');
     });
   });
-  describe('additional unit tests', function(){
-    it('should be around', function(){
-        // TODO: Create these.
-        // Not sure what these should look like yet.
+  describe('additional unit tests', function tests() {
+    it('', function test() {
+      // TODO: Create these.
+      // Not sure what these should look like yet.
     });
   });
 });
