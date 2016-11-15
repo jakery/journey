40a1734abe2d2dc4b0ec8eb5956cde1021566a63
Jacob King <jacobking2@gmail.com>
2016-11-15 11:42:01 -0500

    Beautified player definition.

--- src/Scripts/Sprite/Player.js
+++ src/Scripts/Sprite/Player.js
@@ -1,25 +1,22 @@
-define(
-  'Player',
-  ['./Sprite'],
-  Sprite => function Player(
+define('Player', ['./Sprite'], Sprite => function PlayerModule(
+  game,
+  stage,
+  keyboard,
+  draw,
+  passwordHandler,
+  image
+) {
+  const player = new Sprite.Sprite(
     game,
     stage,
     keyboard,
     draw,
     passwordHandler,
-    image
-  ) {
-    const player = new Sprite.Sprite(
-      game,
-      stage,
-      keyboard,
-      draw,
-      passwordHandler,
-      null
-    );
-    player.imageType = 'image';
-    player.image = image;
-    player.type = 'player';
-    player.player = player;
-    return player;
-  });
+    null
+  );
+  player.imageType = 'image';
+  player.image = image;
+  player.type = 'player';
+  player.player = player;
+  return player;
+});
