3b50c781ecde35e4142c7ee24334657233f8470b
Jacob King <jacobking2@gmail.com>
2017-03-26 12:18:51 -0400

    Moved tests to test directory.

---
+++ test/Coordinates.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable prefer-arrow-callback */
+const Coordinates = require('../src/Scripts/Coordinates');
+
+describe('CoordinatesTest', function CoordinatesTests() {
+  it('should create coordinates.', function test() {
+    const coordinates = new Coordinates(0, 0);
+    assert.equal(0, coordinates.x);
+    assert.equal(0, coordinates.y);
+  });
+
+  it('should have null X and Y properties.', function test() {
+    const coordinates = new Coordinates(Infinity, NaN);
+    assert.isNull(coordinates.x);
+    assert.isNull(coordinates.y);
+  });
+});
---
+++ test/Init.spec.js
@@ -0,0 +1,38 @@
+/* eslint-disable prefer-arrow-callback */
+const Init = require('../Scripts/Init');
+
+describe('Init()', function InitTests() {
+  const mainDiv = document.createElement('div');
+  mainDiv.id = 'main';
+  document.body.appendChild(mainDiv);
+  const myInit = new Init();
+  describe('handleTouchScreen()', function handleTouchScreen() {
+    it('should return true', function test() {
+      assert.isTrue(myInit.handleTouchScreen(true));
+    });
+    it.skip('should return false and not bypass', function test() {
+      // TODO: Add spy for when bypass is set to false.
+    });
+  });
+  describe.skip('checkBrowserSupport()', function checkBrowserSupport() {
+    // TODO: Get modernizr as a package and require that package so that this test works.
+    // it('should return true', function test() {
+    //   assert.isTrue(myInit.checkBrowserSupport());
+    // });
+  });
+
+  describe('checkProtocol()', function checkProtocol() {
+    it('should return true', function test() {
+      assert.isTrue(myInit.checkProtocol());
+    });
+  });
+  describe('setStyle()', function setStyle() {
+    it('should turn off padding on the main div', function test() {
+      myInit.setStyle();
+      assert.equal(mainDiv.style.padding, '0px');
+    });
+  });
+  describe.skip('bypass()', function bypass() {
+    // TODO: Add bypass spy.
+  });
+});
