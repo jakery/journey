2a3c1f5eb9a74dccbb1055b78c45b5cd8f466174
Jacob King <jacobking2@gmail.com>
2017-03-26 14:30:10 -0400

    New path for unit tests.

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/Scripts/**/*.spec.js'
+      'test/**/*.spec.js'
     ],
 
 
