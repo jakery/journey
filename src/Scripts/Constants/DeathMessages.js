886c7a82f1f6fc15ffe567c7349bfa6c83df0a87
Jacob King <jacobking2@gmail.com>
2017-06-22 00:26:47 -0400

    Linted the death messages.

--- src/Scripts/Constants/DeathMessages.js
+++ src/Scripts/Constants/DeathMessages.js
@@ -12,12 +12,12 @@ define('DeathMessages', [], {
   nascar: [
     'You got run over.',
     'Remember Iron Man 2, when Tony Stark and Whip Man fought on the Grand Prix track? This is the same thing, except you died.',
-    "Legally you had the right of way. But you're still dead.",
+    'Legally you had the right of way. But you\'re still dead.',
     'Look both ways before playing in traffic.',
     'You got killed. Best race in years!',
-    "The driver is traumatized. You're so selfish.",
+    'The driver is traumatized. You\'re so selfish.',
     'Remember the car chase from The French Connection? This is the same thing, except you died.',
-    "Great. Now the pizza won't be delivered in 30 minutes or less.",
+    'Great. Now the pizza won\'t be delivered in 30 minutes or less.',
     'One time when I was six I stepped in front of a moving car, just like this. Except I got out of the way.',
 
   ],
@@ -25,16 +25,10 @@ define('DeathMessages', [], {
   britishNascar: [
     'Kate Middleton, forgive us.',
     'Into the boot with you, lad.',
-    "You've wrecked me bonnet.",
-    'Oi, out of the roundabout!',
+    'You\'ve wrecked me bonnet.', 'Oi, out of the roundabout!',
     'You properly bodged that up.',
-    "Typical chav, muckin' about in the road.",
-    'The driver spilled his cuppa on his kit.',
-    "You'll be needin' elastoplast, yobbo.",
-    "You're a right git.",
-    "What are you, the bleedin' lollipop man?",
-    "Remember when the offie worked on spiv's mumpty's carbage to barter for scrumpies, and the plod pulled him out right as the shoes went banger? This is the same thing, except you're brown bread.",
-
+    'Typical chav, muckin\' about in the road.', 'The driver spilled his cuppa on his kit.',
+    'You\'ll be needin\' elastoplast, yobbo.', 'You\'re a right git.', 'What are you, the bleedin\' lollipop man?', 'Remember when the offie worked on spiv\'s mumpty\'s carbage to barter for scrumpies, and the plod pulled him out right as the shoes went banger? This is the same thing, except you\'re brown bread.',
   ],
 
   gronpree: [
@@ -43,17 +37,12 @@ define('DeathMessages', [], {
     'Regarde des deux côtés avant de traverser la rue.\n(Présenté par Google Translate)',
     'Jacques Cousteau a exploré le fond de la occean. Vous êtes à explorer le fond de cette voiture.\n(Présenté par Google Translate)',
     'Ma femme était censé écrire ces messages, mais elle est collée au Canada sans pauses salle de bains.\n(Présenté par Google Translate.)',
-    "Vous rappelez-vous l'époque où Erique préparé un délicieux coq au vin pour sa famille? C'est la même chose, sauf que tu es mort.\n(Présenté par Google Translate)",
-    "Si nous avons roulé comme ça plus souvent, nous n'aurions pas perdu toutes les guerres jamais.\n(Présenté par Google Translate)",
-    'Je suis français. Je viens vous écrasé avec ma voiture. Sentez mes aisselles. Oui, oui!\n(Présenté par Google Translate)',
-    "Comment dites que vous ne \"Sortez de la manière.\" en anglais? Vous n'avez pas. Vous continuez à conduire. :)\n(Présenté par Google Translate)",
-    "Avec les voitures du peuple\nla circulation de progrès\nRemplis ton cœur d'un vin puissant\nEt à demain, tuer un homme\n\n(Présenté par Google Translate)\n(Avec nos excuses à Boublil et Natel)",
-  ],
+    'Vous rappelez-vous l\'époque où Erique préparé un délicieux coq au vin pour sa famille? C\'est la même chose, sauf que tu es mort.\n(Présenté par Google Translate)', 'Si nous avons roulé comme ça plus souvent, nous n\'aurions pas perdu toutes les guerres jamais.\n(Présenté par Google Translate)', 'Je suis français. Je viens vous écrasé avec ma voiture. Sentez mes aisselles. Oui, oui!\n(Présenté par Google Translate)',
+    'Comment dites que vous ne "Sortez de la manière." en anglais? Vous n\'avez pas. Vous continuez à conduire. :)\n(Présenté par Google Translate)', 'Avec les voitures du peuple\nla circulation de progrès\nRemplis ton cœur d\'un vin puissant\nEt à demain, tuer un homme\n\n(Présenté par Google Translate)\n(Avec nos excuses à Boublil et Natel)',],
 
   predator: [
     'You made a friend.',
-    "He's your number one fan.",
-    'I think he likes you.',
+    'He\'s your number one fan.', 'I think he likes you.',
     'Compasses point north. His heart points to you.',
     'He has free candy in his windowless van.',
   ],
@@ -63,19 +52,16 @@ define('DeathMessages', [], {
   ],
 
   time: [
-    "Pick up the pace, grandma.\n\n...that was rude. I'm sorry.",
-    'If you are going to keep taking this long, let us know so I can take a bathroom break.',
+    'Pick up the pace, grandma.\n\n...that was rude. I\'m sorry.', 'If you are going to keep taking this long, let us know so I can take a bathroom break.',
     'Under "areas for improvement", write "time management".',
-    "If you're busy we can try this again later.",
-    'I bet you got a phone call and forgot to pause the game.',
+    'If you\'re busy we can try this again later.', 'I bet you got a phone call and forgot to pause the game.',
   ],
 
   miscDeath: [
     'FYI, there is no achievement for failing a billion times.',
-    "It looks like you're having trouble with this level. Pay $4.99 now to ride Artie The Magic Dung Beetle to the exit.",
-    'I made this level way too tough. Sorry.',
+    'It looks like you\'re having trouble with this level. Pay $4.99 now to ride Artie The Magic Dung Beetle to the exit.', 'I made this level way too tough. Sorry.',
     'Where did you learn to play video games--the toilet store?',
-    "This time it wasn't your fault.",
+    'This time it wasn\'t your fault.',
   ],
 
   water: [
@@ -92,23 +78,18 @@ define('DeathMessages', [], {
     'A wall appeared where you are. Bummer.',
     'Try not stand where the walls show up. You are very dead.',
     'Remember Star Wars, when they were trapped in the garbage smasher on the detention level? This is the same thing, except you died.',
-    "You're now very skinny, like a pancake.",
+    'You\'re now very skinny, like a pancake.',
     'Wall, meet human. Human, wall.',
     'Who put that there?',
     'The wall apparated with you in it.',
   ],
 
   help: [
-    "Don't ask me how, but you're dead.",
-  ],
+    'Don\'t ask me how, but you\'re dead.',],
 
   win: [
     'You found the warp zone\nto level [currentLevel + 1]!',
-    "YOU'RE WINNER",
-    'Huzzah!',
-    "Keep Goin'? Off course!",
-    'A WINNER IS YOU.',
-
+    'YOU\'RE WINNER', 'Huzzah!',
+    'Keep Goin\'? Off course!', 'A WINNER IS YOU.',
   ],
-
 });
