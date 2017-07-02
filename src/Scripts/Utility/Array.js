f49a5e4de44346eb8adb80ead0d2a7cd00ed772f
Jacob King <jacobking2@gmail.com>
2017-07-02 15:56:05 -0400

    Removed TODO

--- src/Scripts/Utility/Array.js
+++ src/Scripts/Utility/Array.js
@@ -1,4 +1,3 @@
-// TODO: Move this to jaja.js
 define('ArrayExtensions', [], () => function Array() {
   this.removeBySpriteId = function removeBySpriteId(array, id, safe) {
     if (typeof array === 'undefined') throw new Error('No array was provided.');
