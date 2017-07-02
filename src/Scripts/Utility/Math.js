55c76adddeb1cc72c2d03b96839f5b837dbe31a3
Jacob King <jacobking2@gmail.com>
2017-07-02 16:08:04 -0400

    Removed unnecessary utility files.

--- src/Scripts/Utility/Math.js
+++
@@ -1,6 +0,0 @@
-define('Math', [], {
-  isNumeric: variable => !isNaN(parseFloat(variable)) && isFinite(variable),
-  toRadians: degrees => (degrees * Math.PI) / 180,
-  toDegrees: radians => (radians * 180) / Math.PI,
-  TAU: 2 * Math.PI,
-});
